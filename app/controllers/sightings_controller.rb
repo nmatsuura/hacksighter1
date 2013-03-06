6acfeb028792b971bdea22da25d7a43017c804bd
naomimatsuura <matsuura.naomi@gmail.com>
2013-03-05 22:36:37 -0500

    Added a new and create action and viewers

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -4,4 +4,19 @@ class SightingsController < ApplicationController
 		@sightings = Sighting.all
 	end
 
+	def new
+		@sighting = Sighting.new
+	end
+
+	def create
+		@sighting = Sighting.new(params[:sighting])
+		if 
+			@sighting.valid?
+			@sighting.save
+			redirect_to sightings_path
+		else
+			render :new
+		end
+	end
+
 end
