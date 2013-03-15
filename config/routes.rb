8f4cacf802fe23318348fa161a735c54b5d46a5e
naomimatsuura <matsuura.naomi@gmail.com>
2013-03-14 21:02:46 -0400

    Removing users

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,6 @@ Hacksighter1::Application.routes.draw do
     resources :comments
   end
 
-  # resources :users do
-  #   resources :posts
-  # end
 
   root to: 'sightings#index'
   # The priority is based upon order of creation:
