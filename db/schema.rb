aa7bd027a0b61b942f1b5b03519ae1eca0145e17
naomimatsuura <matsuura.naomi@gmail.com>
2013-04-05 10:40:12 -0400

    Added some photos to db, changed show page slightly

--- db/schema.rb
+++ db/schema.rb
@@ -58,6 +58,8 @@ ActiveRecord::Schema.define(:version => 20130405001852) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "uid"
+    t.string   "provider"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
